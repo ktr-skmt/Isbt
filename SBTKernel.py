178187f4524a4dbf4d97434b2c19f039ab67f1b7
Kotaro Sakamoto <sakamoto@forest.eis.ynu.ac.jp>
2017-09-18 18:11:30 +0900

    Refactored

--- SBTKernel.py
+++ SBTKernel.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from ipykernel.kernelbase import Kernel
 from telnetlib import Telnet
 
@@ -10,37 +11,30 @@ PORT = 12700
 ENCODING = 'ascii'
 
 crlf_pattern = re.compile(r'[\r\n]+')
-problem_ew_regex = r'{"type":"xsbti.Problem","message":{"category":"","severity":"%s","message":"(?P<message>[^"]+)","position":{"line":(?P<position_line>\d+),"lineContent":"(?P<position_lineContent>[^"]+)","offset":(?P<position_offset>\d+),"pointer":(?P<position_pointer>\d+),"pointerSpace":"(?P<position_pointerSpace> *)","sourcePath":"(?P<position_sourcePath>[^"]+)","sourceFile":"file:(?P<position_sourceFile>[^"]+)"}},"level":"%s","channelName":"channel-\d+","execId":"[^"]+"}'
-problem_error_regex = problem_ew_regex % ('Error', 'error')
-problem_warn_regex = problem_ew_regex % ('Warn', 'warn')
-problem_error_pattern = re.compile(problem_error_regex)
-problem_warn_pattern = re.compile(problem_warn_regex)
-string_event_pattern = re.compile(
-    r'{"type":"StringEvent","level":"info","message":"(?P<message>[^"]+)","channelName":"channel-\d+","execId":"[^"]+"}')
-ew_regex = r'{"type":"StringEvent","level":"%s","message":"(?P<message>[^"]+)","channelName":"channel-\d+","execId":"[^"]+"}'
-error_regex = ew_regex % 'error'
-warn_regex = ew_regex % 'warn'
-error_pattern = re.compile(error_regex)
-warn_pattern = re.compile(warn_regex)
+problem_regex = r'{"type":"xsbti.Problem","message":{"category":"","severity":"%s","message":"(?P<message>[^"]+)","position":{"line":(?P<position_line>\d+),"lineContent":"(?P<position_lineContent>[^"]+)","offset":(?P<position_offset>\d+),"pointer":(?P<position_pointer>\d+),"pointerSpace":"(?P<position_pointerSpace> *)","sourcePath":"(?P<position_sourcePath>[^"]+)","sourceFile":"file:(?P<position_sourceFile>[^"]+)"}},"level":"%s","channelName":"channel-\d+","execId":"[^"]+"}'
+problem_error_pattern = re.compile(problem_regex % ('Error', 'error'))
+problem_warn_pattern = re.compile(problem_regex % ('Warn', 'warn'))
+string_event_regex = r'{"type":"StringEvent","level":"%s","message":"(?P<message>[^"]+)","channelName":"channel-\d+","execId":"[^"]+"}'
+error_pattern = re.compile(string_event_regex % 'error')
+warn_pattern = re.compile(string_event_regex % 'warn')
+info_pattern = re.compile(string_event_regex % 'info')
 
 
-def matcher_to_message(message: str, line: str, line_content: str, offset: str,
-                       pointer: str, pointer_space: str, source_path: str, source_file: str, is_error: bool) -> str:
-    ret = ''
-    if is_error:
-        ret += '[\x1b[31;1merror'
-    else:
-        ret += '[\x1b[33;1mwarn'
-    ret += '\x1b[30m] '
-    ret += message + '\n'
-    ret += 'line: %s\n' % line
-    ret += 'line content: %s\n' % line_content
-    ret += 'offset: %s\n' % offset
-    ret += 'pointer: %s\n' % pointer
-    ret += 'pointer space: "%s"\n' % pointer_space
-    ret += 'source path: %s\n' % source_path
-    ret += 'source file: %s\n' % source_file
-    return ret
+class Status(Enum):
+    Success = '[\x1b[32;1msuccess\x1b[30m] '
+    Warn = '[\x1b[33;1mwarn\x1b[30m] '
+    Error = '[\x1b[31;1merror\x1b[30m] '
+    Info = '[info] '
+    Help = '[help] '
+    Debug = '[debug] '
+
+
+class RunMode(Enum):
+    Regular = 0
+    Debug = 1
+
+
+RUN_MODE = RunMode.Regular
 
 
 class SBTKernel(Kernel):
@@ -58,10 +52,11 @@ class SBTKernel(Kernel):
     def __init__(self, **kwargs):
         Kernel.__init__(self, **kwargs)
         self.silent = None
+        self.has_no_error = True
 
     def process_output(self, output):
         if not self.silent:
-            self.send_response(self.iopub_socket, 'stream', {'name': 'stdout', 'text': output})
+            self.send_response(self.iopub_socket, 'stream', {'name': 'stdout', 'text': output + '\n'})
 
     def _start_sbt(self, host, port):
         sig = signal.signal(signal.SIGINT, signal.SIG_DFL)
@@ -78,20 +73,26 @@ class SBTKernel(Kernel):
         """DEPRECATED"""
         raise NotImplementedError
 
-    def do_execute(self, code, silent, store_history=True,
-                   user_expressions=None, allow_stdin=False):
+    def do_execute(self, code, silent, store_history=True, user_expressions=None, allow_stdin=False):
         command = crlf_pattern.sub('', code.strip())
+        ok = {
+            'status': 'ok',
+            'execution_count': self.execution_count,
+            'payload': [],
+            'user_expressions': {}
+        }
         if command.startswith('sbt-server'):
             tokens = command.split(' ')
             if len(tokens) == 1:
                 self._start_sbt(HOST, PORT)
             elif len(tokens) == 2 and tokens[1] == 'help':
+                header = Status.Help.value
                 self.process_output(
-                    '[help] sbt-server needs an argument pair of host and port. For example, "sbt-server localhost 334"\n')
+                    header + 'sbt-server needs an argument pair of host and port. For example, "sbt-server localhost 334"\n')
                 self.process_output(
-                    '[help] When sbt-server has no argument, run "sbt-server %s %d" as default\n' % (HOST, PORT))
+                    header + 'When sbt-server has no argument, run "sbt-server %s %d" as default\n' % (HOST, PORT))
                 self.process_output(
-                    '[help] You can add a dedicated port as "serverPort := 12700" in your build.sbt\n')
+                    header + 'You can add a dedicated port as "serverPort := 12700" in your build.sbt\n')
             elif len(tokens) == 3:
                 self._start_sbt(tokens[1], tokens[2])
             else:
@@ -101,78 +102,105 @@ class SBTKernel(Kernel):
                     'evalue': 'sbt-server needs an argument pair of host and port. For example, "sbt-server localhost 12700"',
                     'traceback': None
                 }
-            return {
-                'status': 'ok',
-                'execution_count': self.execution_count,
-                'payload': [],
-                'user_expressions': {}
-            }
+            return ok
         code = '{"type":"ExecCommand","commandLine":"%s"}\n' % command
         if not code:
-            return {
-                'status': 'ok',
-                'execution_count': self.execution_count,
-                'payload': [],
-                'user_expressions': {}
-            }
+            return ok
         start_time = datetime.datetime.today()
         self.wrapper.write(code.encode(ENCODING))
         done = '{"type":"ExecStatusEvent","status":"Done","commandQueue":["%s","shell"]}' % command
-        has_no_error = True
+        self.has_no_error = True
         line = ''
+
         if not silent:
+            def display_json_to_debug():
+                if RUN_MODE is RunMode.Debug:
+                    self.send_response(self.iopub_socket, 'stream', {
+                        'name': 'stdout',
+                        'text': Status.Debug.value + line + '\n'
+                    })
             while not line == done:
                 line = self.wrapper.read_until('\n'.encode(ENCODING)).decode(ENCODING).strip()
-            while not (line.endswith('"commandQueue":["shell"]}') or line.endswith('"commandQueue":["shell"]}\n')):
-                line = crlf_pattern.sub('', self.wrapper.read_until('\n'.encode(ENCODING)).decode(
+                display_json_to_debug()
+            while not line.endswith('"commandQueue":["shell"]}'):
+                line = crlf_pattern.sub('\n', self.wrapper.read_until('\n'.encode(ENCODING)).decode(
                     ENCODING).strip()).replace(r'\u001b', '\x1b')
-                # self.send_response(self.iopub_socket, 'stream', {
-                #    'name': 'stdout',
-                #    'text': 'LINE\n' + line + '\n'
-                # })
-                matcher = string_event_pattern.match(line)
-                if matcher is not None:
-                    self.process_output('[info] %s\n' % matcher.group('message'))
+                display_json_to_debug()
 
-                def m2m(is_e: bool):
-                    self.process_output(matcher_to_message(
-                        matcher.group('message'),
-                        matcher.group('position_line'),
-                        matcher.group('position_lineContent'),
-                        matcher.group('position_offset'),
-                        matcher.group('position_pointer'),
-                        matcher.group('position_pointerSpace'),
-                        matcher.group('position_sourcePath'),
-                        matcher.group('position_sourceFile'),
-                        is_error=is_e
-                    ))
-                matcher = problem_warn_pattern.match(line)
-                if matcher is not None:
-                    m2m(is_e=False)
-                matcher = problem_error_pattern.match(line)
-                if matcher is not None:
-                    has_no_error = False
-                    m2m(is_e=True)
-                matcher = warn_pattern.match(line)
-                if matcher is not None:
-                    self.process_output('[\x1b[33;1mwarn\x1b[30m] %s\n' % matcher.group('message'))
-                matcher = error_pattern.match(line)
-                if matcher is not None:
-                    self.process_output('[\x1b[31;1merror\x1b[30m] %s\n' % matcher.group('message'))
+                def to_message(status: Status,
+                               message: str,
+                               line_: str,
+                               line_content: str,
+                               offset: str,
+                               pointer: str,
+                               pointer_space: str,
+                               source_path: str,
+                               source_file: str) -> str:
+                    ret = status.value
+                    ret += message + '\n'
+                    ret += 'line: %s\n' % line_
+                    ret += 'line content: %s\n' % line_content
+                    ret += 'offset: %s\n' % offset
+                    ret += 'pointer: %s\n' % pointer
+                    ret += 'pointer space: "%s"\n' % pointer_space
+                    ret += 'source path: %s\n' % source_path
+                    ret += 'source file: %s\n' % source_file
+                    return ret.strip()
+
+                def string_event_to_message(status: Status):
+                    pattern = None
+                    if status == Status.Info:
+                        pattern = info_pattern
+                    elif status == Status.Warn:
+                        pattern = warn_pattern
+                    elif status == Status.Error:
+                        pattern = error_pattern
+                    if pattern is not None:
+                        matcher = pattern.match(line)
+                        if matcher is not None:
+                            self.process_output(status.value + matcher.group('message'))
+                            if status == Status.Error:
+                                self.has_no_error = False
+
+                string_event_to_message(Status.Info)
+                string_event_to_message(Status.Warn)
+                string_event_to_message(Status.Error)
+
+                def problem_to_message(status: Status):
+                    pattern = None
+                    if status == Status.Error:
+                        pattern = problem_error_pattern
+                    elif status == Status.Warn:
+                        pattern = problem_warn_pattern
+                    if pattern is not None:
+                        matcher = pattern.match(line)
+                        if matcher is not None:
+                            self.process_output(to_message(
+                                status,
+                                matcher.group('message'),
+                                matcher.group('position_line'),
+                                matcher.group('position_lineContent'),
+                                matcher.group('position_offset'),
+                                matcher.group('position_pointer'),
+                                matcher.group('position_pointerSpace'),
+                                matcher.group('position_sourcePath'),
+                                matcher.group('position_sourceFile')
+                            ))
+                            if status == Status.Error:
+                                self.has_no_error = False
+
+                problem_to_message(Status.Warn)
+                problem_to_message(Status.Error)
+
             completed_time = datetime.datetime.today()
             elapsed_time = completed_time - start_time
-            completed = ' Total time: %s, completed %s\n' % (elapsed_time, completed_time)
+            completed = 'Total time: %s, completed %s' % (elapsed_time, completed_time)
 
-            if has_no_error:
-                self.process_output('[\x1b[32;1msuccess\x1b[30m]' + completed)
+            if self.has_no_error:
+                self.process_output(Status.Success.value + completed)
             else:
-                self.process_output('[\x1b[31;1merror\x1b[30m]' + completed)
-        return {
-            'status': 'ok',
-            'execution_count': self.execution_count,
-            'payload': [],
-            'user_expressions': {}
-        }
+                self.process_output(Status.Error.value + completed)
+        return ok
 
 
 # ===== MAIN =====
